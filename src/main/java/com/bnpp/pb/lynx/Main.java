0d364ac71414e3aab65912210bd98c668915eb60
Your Name <your.email@example.com>
2025-04-04 10:20:46 +0530

    Fix Spring Boot configuration issues

---
+++ src/main/java/com/bnpp/pb/lynx/Main.java
@@ -0,0 +1,21 @@
+package com.bnpp.pb.lynx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource({"classpath:spring/application-config.xml", "classpath:spring/mvc-config.xml"})
+public class Main extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Main.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+} 
--- src/main/java/com/bnpp/pb/lynx/config/AppConfig.java
+++ src/main/java/com/bnpp/pb/lynx/config/AppConfig.java
@@ -12,19 +12,20 @@ import com.bnpp.pb.lynx.service.SubjectService;
 import com.bnpp.pb.lynx.service.impl.MarksServiceImpl;
 import com.bnpp.pb.lynx.service.impl.StudentServiceImpl;
 import com.bnpp.pb.lynx.service.impl.SubjectServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 
 @Configuration
-@ImportResource("classpath:spring/application-config.xml")
 public class AppConfig {
     
     @Bean
+    @ConditionalOnMissingBean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
@@ -35,49 +36,50 @@ public class AppConfig {
     }
     
     @Bean
-    public JdbcTemplate jdbcTemplate() {
-        return new JdbcTemplate(dataSource());
+    @ConditionalOnMissingBean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
     
     @Bean
-    public StudentDao studentDao() {
+    public StudentDao studentDao(JdbcTemplate jdbcTemplate) {
         StudentDaoImpl studentDao = new StudentDaoImpl();
-        studentDao.setJdbcTemplate(jdbcTemplate());
+        studentDao.setJdbcTemplate(jdbcTemplate);
         return studentDao;
     }
     
     @Bean
-    public SubjectDao subjectDao() {
+    public SubjectDao subjectDao(JdbcTemplate jdbcTemplate) {
         SubjectDaoImpl subjectDao = new SubjectDaoImpl();
-        subjectDao.setJdbcTemplate(jdbcTemplate());
+        subjectDao.setJdbcTemplate(jdbcTemplate);
         return subjectDao;
     }
     
     @Bean
-    public MarksDao marksDao() {
+    public MarksDao marksDao(JdbcTemplate jdbcTemplate) {
         MarksDaoImpl marksDao = new MarksDaoImpl();
-        marksDao.setJdbcTemplate(jdbcTemplate());
+        marksDao.setJdbcTemplate(jdbcTemplate);
         return marksDao;
     }
     
     @Bean
-    public StudentService studentService() {
+    public StudentService studentService(StudentDao studentDao) {
         StudentServiceImpl studentService = new StudentServiceImpl();
-        studentService.setStudentDao(studentDao());
+        studentService.setStudentDao(studentDao);
         return studentService;
     }
     
     @Bean
-    public SubjectService subjectService() {
+    public SubjectService subjectService(SubjectDao subjectDao) {
         SubjectServiceImpl subjectService = new SubjectServiceImpl();
-        subjectService.setSubjectDao(subjectDao());
+        subjectService.setSubjectDao(subjectDao);
         return subjectService;
     }
     
     @Bean
-    public MarksService marksService() {
+    public MarksService marksService(MarksDao marksDao) {
         MarksServiceImpl marksService = new MarksServiceImpl();
-        marksService.setMarksDao(marksDao());
+        marksService.setMarksDao(marksDao);
         return marksService;
     }
 } 
